da79e5fbacc2473f2dc180e694686b4807effca7
Roland4444 <turbopollux@rambler.ru>
2022-06-18 00:33:22 +0400

    initial

--- src/Basic.java
+++ src/Basic.java
@@ -1,6 +1,7 @@
 public class Basic {
     public static void main(String[] args){
         System.out.println("Avanasiev troinoy pidor!");
+        System.out.println("Avanasiev:: ya ne pidor!");
         System.out.println("Avanasiev troinoy pidor!");
     };
 }
